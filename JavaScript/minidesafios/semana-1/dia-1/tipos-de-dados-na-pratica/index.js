4e0815da98b980e47214f7b998714fccb6ec9c59
Campelo <josecampelojr@gmail.com>
2025-04-11 07:56:58 -0300

    Resolução do Minidesafio de Tipos de Dados: Sistema de Pontuação de Partida

---
+++ JavaScript/minidesafios/semana-1/dia-1/tipos-de-dados-na-pratica/index.js
@@ -0,0 +1,20 @@
+const nomePersonagem = "Kha'Zix"
+let nivelPersonagem = 16
+let quantidadeAbates = 21
+let quantidadeMortes = 2
+let isKDAPerfeito = null
+let isMVP = null;
+let isStatusCarregado = undefined;
+
+console.log(`${nomePersonagem} finalizou a partida no nível ${nivelPersonagem} com ${quantidadeAbates} abates e ${quantidadeMortes} mortes.`)
+console.log(`KDA Perfeito? ${isKDAPerfeito}`)
+console.log(`MVP da Partida? ${isMVP}`)
+console.log(`Status de carregamento: ${isStatusCarregado}`)
+
+isKDAPerfeito = true
+isMVP = true;
+isStatusCarregado = true;
+console.log("---------------- Status Carregado com sucesso! ----------------")
+console.log(`Status de carregamento: ${isStatusCarregado}`)
+console.log(`KDA Perfeito? ${isKDAPerfeito}`)
+console.log(`MVP da Partida? ${isMVP}`)
