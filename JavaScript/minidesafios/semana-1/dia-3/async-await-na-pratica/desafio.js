20b746dc5af82ad7fd9af181dcb0db1d69a279ba
Campelo <josecampelojr@gmail.com>
2025-04-19 19:25:20 -0300

    Resolução do desafio de Async/Await: Desafio: Preparação de Time Ranqueado com async/await

---
+++ JavaScript/minidesafios/semana-1/dia-3/async-await-na-pratica/desafio.js
@@ -0,0 +1,52 @@
+// Desafio: Preparação de Time Ranqueado com async/await
+
+const jogadores = [
+    { nome: "Faker", campeao: "Ahri" },
+    { nome: "brTT", campeao: "Lucian" },
+    { nome: "Rekkles", campeao: "Ezreal" },
+    { nome: "Caps", campeao: "LeBlanc" },
+    { nome: "Jankos", campeao: "Lee Sin" }
+]
+
+function escolherCampeao(jogador) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`Jogador ${jogador.nome} escolheu ${jogador.campeao}`)
+            resolve(true)
+        }, 1000)
+    })
+}
+
+function confirmarParticipacao(jogador) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`Jogador ${jogador.nome} confirmou participação`)
+            resolve(true)
+        }, 1000)
+    })
+}
+
+function carregarJogo(jogador) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`Jogador ${jogador.nome} está carregando o jogo...`)
+            resolve(true)
+        }, 1000)
+    })
+}
+
+async function preparaJogador(jogador) {
+    console.log(`Preparando o jogador ${jogador.nome}`)
+    await confirmarParticipacao(jogador)
+    await escolherCampeao(jogador)
+    await carregarJogo(jogador)
+    console.log()
+}
+
+async function prepararEquipe(jogadores) {
+    for(let i = 0; i < jogadores.length; i++) {
+        await preparaJogador(jogadores[i])
+    }
+}
+
+prepararEquipe(jogadores)
